2e797e0e80ebce178e7ac33af1c5e140943daf45
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-05-07 08:05:24 +0800

    CRA

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      Hello world
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
